1c09e3f603eddd3e2792bd48fc3907c050b18c73
Matthew Colvin <35540398+MatthewColvin@users.noreply.github.com>
2025-04-05 17:54:27 +0000

    Quick fix for mingw posix socket API

--- examples/templates/posix_sockets.h
+++ examples/templates/posix_sockets.h
@@ -58,7 +58,7 @@ int open_nb_socket(const char* addr, const char* port) {
     if (sockfd != -1) fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL) | O_NONBLOCK);
 #else
     if (sockfd != INVALID_SOCKET) {
-        int iMode = 1;
+        u_long iMode = 1;
         ioctlsocket(sockfd, FIONBIO, &iMode);
     }
 #endif
